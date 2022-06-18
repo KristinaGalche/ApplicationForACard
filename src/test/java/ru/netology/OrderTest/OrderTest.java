08ca532100d09a48da024dbb64bbfdd3064656be
KristinaGalche <hit-tb@mail.ru>
2022-06-19 01:30:29 +1000

    delete finish test

--- src/test/java/ru/netology/OrderTest/OrderTest.java
+++ src/test/java/ru/netology/OrderTest/OrderTest.java
@@ -95,9 +95,4 @@ public class OrderTest {
         $("[type=button]").click();
         $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
-
-    @AfterEach
-    public void closeApp() {
-        closeWindow();
-    }
 }
